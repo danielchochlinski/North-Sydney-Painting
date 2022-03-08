e560b36926ffd3775009ce644bdb0bfbb0cf23db
danielchochlinski <89450753+danielchochlinski@users.noreply.github.com>
2022-03-08 14:30:24 +0100

    3/8/22

--- components/contact/ContactForm.jsx
+++ components/contact/ContactForm.jsx
@@ -32,7 +32,7 @@ function ContactForm(props) {
         </div>
         <div className="class">
           <img src="images/icons/email.png" alt="email" />
-          <h4>jacobpuertasalvatore@gmail.com</h4>
+          <h4>northsydneypainting@outlook.com</h4>
           <p>Mon-Sat 24/7</p>
           <a
             href="mailto: jacobpuertasalvatore@gmail.com"
