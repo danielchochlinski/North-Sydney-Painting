dc5a39548bcf4b1853eb51fd1ef5456777f0ae5f
danielchochlinski <89450753+danielchochlinski@users.noreply.github.com>
2022-03-09 16:12:44 +0100

    3/9/22

--- pages/api/submitContactForm.js
+++ pages/api/submitContactForm.js
@@ -3,28 +3,24 @@ const key = process.env.SENDGRID_API_KEY;
 
 mail.setApiKey(key);
 
-export default async function handler(req, res) {
-  try {
-    const body = JSON.parse(req.body);
+export default (req, res) => {
+  const body = JSON.parse(req.body);
 
-    const message = `
+  const message = `
   Name: ${body.name}<br/>
   Email: ${body.email}<br/>
   PhoneNumber: ${body.phone}<br/>
   Message: ${body.message}<br/>`;
 
-    const data = {
-      to: "jacobpuertasalvatore@gmail.com",
-      from: "NorthSydneyPainting@northsydneypainting.com",
-      subject: "New business inquiry!",
-      test: message,
-      html: message,
-    };
-    console.log(data);
-    mail.send(data);
+  const data = {
+    to: "jacobpuertasalvatore@gmail.com",
+    from: "NorthSydneyPainting@northsydneypainting.com",
+    subject: "New business inquiry!",
+    test: message,
+    html: message,
+  };
+  console.log(data);
+  mail.send(data);
 
-    res.status(200).json({ status: "Ok" });
-  } catch (error) {
-    console.log(error);
-  }
-}
+  res.status(200).json({ status: "Ok" });
+};
